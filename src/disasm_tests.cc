f0cdfa4e7fdf1f04adaa21061f5216f337d72b67
John W. Terrell <john@coolpeoplenetworks.com>
2022-02-27 20:12:09 -0800

    Fixed sign extnesion issues on branch instructins

--- src/disasm_tests.cc
+++ src/disasm_tests.cc
@@ -4,12 +4,23 @@
 TEST_CASE( "Simple disassembly test", "[disassembly]" ) {
     auto disassembler = disassembler_t(32);
 
-    const uint64_t u = 0xee800000;
-    const int64_t s = (int32_t)u >> 20;
-    REQUIRE(s == -280); 
+    {
+        const uint64_t u = 0xee800000;
+        const int64_t s = (int32_t)u >> 20;
+        REQUIRE(s == -280); 
+    }
 
-    const uint32_t instructionBits = 0xee828293;
-    insn_t instruction(instructionBits);
-    auto disassembly = disassembler.disassemble(instruction);
-    REQUIRE(disassembly == "addi    t0, t0, -280");
+    {
+        const uint32_t instructionBits = 0xee828293;
+        insn_t instruction(instructionBits);
+        auto disassembly = disassembler.disassemble(instruction);
+        REQUIRE(disassembly == "addi    t0, t0, -280");
+    }
+
+    {
+        const uint32_t instructionBits = 0xfc9296e3;
+        insn_t instruction(instructionBits);
+        auto disassembly = disassembler.disassemble(instruction);
+        REQUIRE(disassembly == "bne     t0, s1, pc - 52");
+    }
 }
